bcb4ed13e6e610c6d2ac10002f48d0af83706625
HaileB65 <brendon.haile@gmail.com>
2022-07-11 18:41:55 -0500

    creating local repository

---
+++ src/main/java/Demo_Consuming_Thirdparty_API/MostPopularNYTArticlesAPP/MostPopularNytArticlesAppApplication.java
@@ -0,0 +1,13 @@
+package Demo_Consuming_Thirdparty_API.MostPopularNYTArticlesAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostPopularNytArticlesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostPopularNytArticlesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Demo_Consuming_Thirdparty_API/MostPopularNYTArticlesAPP/MostPopularNytArticlesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Demo_Consuming_Thirdparty_API.MostPopularNYTArticlesAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MostPopularNytArticlesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
