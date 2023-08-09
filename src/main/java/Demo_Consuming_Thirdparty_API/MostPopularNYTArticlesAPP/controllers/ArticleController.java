ae7081b911e911c4c08ecbaf9ffd4e3196c36539
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-08-08 20:01:39 -0500

    Created docker image and container to run MostPopularNYTArticles application.

--- src/main/java/Demo_Consuming_Thirdparty_API/MostPopularNYTArticlesAPP/controllers/ArticleController.java
+++ src/main/java/Demo_Consuming_Thirdparty_API/MostPopularNYTArticlesAPP/controllers/ArticleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/articles") // starting end point in browser
+@RequestMapping("/")
 public class ArticleController {
 
     @Autowired
